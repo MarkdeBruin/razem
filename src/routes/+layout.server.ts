33b3366511bd7e1d4b5d6be776a49d8c37d773d7
MarkdeBruin <contact@mrkdbrn.com>
2026-03-27 09:34:09 +0100

    Moved roles from layout.server to locals

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,9 @@
-import { getAllUsers } from '$lib/services/users';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const currentUser = locals.currentUser;
-
-	const users = await getAllUsers();
-	const owner = users.find((user) => user.role === 'owner')!;
-	const partner = users.find((user) => user.role === 'partner')!;
+	const owner = locals.owner;
+	const partner = locals.partner;
 
 	return { currentUser, owner, partner };
-}
+};
