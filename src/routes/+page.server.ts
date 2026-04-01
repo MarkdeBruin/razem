c4df68415ddc3016a8a546876bb89603137622a0
MarkdeBruin <contact@mrkdbrn.com>
2026-04-01 12:12:49 +0200

    Removed unused code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,8 @@
 import { getAllLedgers } from '$lib/services/ledgers';
-import { deleteLedger } from '$lib/services/ledgers';
-import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {
 		ledgers: await getAllLedgers()
 	};
 };
-
-export const actions = {
-	'delete-ledger': async ({ request }) => {
-    const data = await request.formData();
-    
-    const id = data.get('id') as string
-    if (!id) return fail(422, { error: 'Id is required' });
-		
-		await deleteLedger(id);
-		redirect(303, '/');
-	}
-} satisfies Actions;
