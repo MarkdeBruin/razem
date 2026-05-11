ce540fe1e0e71a3a9d535c438d3279e873874b2f
MarkdeBruin <contact@mrkdbrn.com>
2026-05-11 17:06:26 +0200

    Categories & Keywords page styles start

--- src/routes/settings/categories/+page.svelte
+++ src/routes/settings/categories/+page.svelte
@@ -2,73 +2,68 @@
 	import type { PageProps } from './$types';
 	import { enhance } from '$app/forms';
 	import { matchCategory } from '$lib/utils/categories';
+	import { ArrowLeftIcon, QuestionMarkIcon } from 'phosphor-svelte';
 
 	let { data, form }: PageProps = $props();
 	let keyword = $state('');
 	let isDuplicate = $state(false);
 </script>
 
-<header>
-	<hgroup>
-		<h1>Categories & Keywords</h1>
-		<p>Keywords are used to automatically select the right category when adding an expense.</p>
-	</hgroup>
+<header class="header-sticky--back">
+	<a href="/" class="btn--circle" aria-label="Back home"><ArrowLeftIcon /></a>
+	<h1>Categories & Keywords</h1>
+	<button class="btn--circle" aria-label="Help"><QuestionMarkIcon /></button>
 </header>
 
-<main>
-	<section>
-		<form method="POST" use:enhance>
-			<label>
-				<span>Keyword</span>
-				<input
-					type="text"
-					name="keyword"
-					bind:value={keyword}
-					required
-					autocapitalize="sentences"
-					oninput={() => {
-						isDuplicate = !!matchCategory(keyword, data.categories);
-					}}
-				/>
-				{#if isDuplicate}
-					<small>"{keyword}" already exists</small>
-				{/if}
-				{#if form?.keywordMissing}
-					<small>Keyword is required</small>
-				{/if}
-			</label>
-
-			<fieldset>
-				<legend>Category</legend>
-				{#each data.categories as category (category.id)}
-					<label>
-						<input type="radio" name="category" value={category.id} required />
-						{category.name}
-					</label>
-				{/each}
-			</fieldset>
-			{#if form?.categoryMissing}
-				<small>Please select a category</small>
+<main class="stack">
+	<form class="margin-block-end-double" method="POST" use:enhance>
+		<h2>New keyword</h2>
+		<label>
+			Keyword
+			<input
+				type="text"
+				name="keyword"
+				bind:value={keyword}
+				required
+				autocapitalize="sentences"
+				oninput={() => {
+					isDuplicate = !!matchCategory(keyword, data.categories);
+				}}
+			/>
+			{#if isDuplicate}
+				<small>“{keyword}” already exists</small>
 			{/if}
+			{#if form?.keywordMissing}
+				<small>Keyword is required</small>
+			{/if}
+		</label>
 
-			<button type="submit" disabled={isDuplicate}> Add keyword </button>
-		</form>
-	</section>
-	<hr />
-	<section>
-		<ul>
+		<fieldset>
+			<legend>Category</legend>
 			{#each data.categories as category (category.id)}
-				<li>
-					<p>{category.name}</p>
-					{#if category.keywords.length > 0}
-						<ul>
-							{#each category.keywords as keyword}
-								<li>{keyword}</li>
-							{/each}
-						</ul>
-					{/if}
-				</li>
+				<label>
+					<input type="radio" name="category" value={category.id} required />
+					{category.name}
+				</label>
 			{/each}
-		</ul>
-	</section>
+		</fieldset>
+		{#if form?.categoryMissing}
+			<small>Please select a category</small>
+		{/if}
+
+		<input class="btn" type="submit" value="Add keyword" disabled={isDuplicate} />
+	</form>
+
+	{#each data.categories as category (category.id)}
+		<section class="list--section">
+			<header>
+				<h2>{category.name}</h2>
+			</header>
+			{#if category.keywords.length > 0}<ul>
+					{#each category.keywords as keyword}
+						<li><p>{keyword}</p></li>
+					{/each}
+				</ul>{/if}
+		</section>
+	{/each}
 </main>
