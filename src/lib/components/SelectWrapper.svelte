75e0c061d86e8be3f0c98afce13ecbacf3aeb6af
MarkdeBruin <contact@mrkdbrn.com>
2026-05-15 12:27:27 +0200

    SelectWrapper for better custom icon styling

---
+++ src/lib/components/SelectWrapper.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+	import { CaretUpDownIcon } from 'phosphor-svelte';
+</script>
+
+<div class="select-wrapper">
+	{@render children()}
+	<CaretUpDownIcon />
+</div>
