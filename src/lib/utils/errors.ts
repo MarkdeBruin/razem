b880c917567837221904409224ff9c9cb652f878
MarkdeBruin <contact@mrkdbrn.com>
2026-09-08 10:30:26 +0200

    Improved NotFoundError() util

--- src/lib/utils/errors.ts
+++ src/lib/utils/errors.ts
@@ -1,5 +1,10 @@
-export function notFoundError(entity: string, id?: string): Error {
-  if (id) return new Error(`${entity} with id "${id}" not found`);
-	
-	return new Error(`${entity} not found`);
+export class NotFoundError extends Error {
+	constructor(entity: string, id?: string) {
+		super(id ? `${entity} with id "${id}" not found` : `${entity} not found`);
+		this.name = 'NotFoundError';
+	}
+}
+
+export function notFoundError(entity: string, id?: string): NotFoundError {
+	return new NotFoundError(entity, id);
 }
